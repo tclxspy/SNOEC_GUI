fb9e9afdf75ada04138b223a620fff7186f01231
Nicholas <603104212@qq.com>
2017-05-16 17:13:47 +0800

    add ico files to show status

--- Code/SNOEC_GUI/SNOEC_GUI/Properties/Resources.Designer.cs
+++ Code/SNOEC_GUI/SNOEC_GUI/Properties/Resources.Designer.cs
@@ -71,6 +71,26 @@ namespace SNOEC_GUI.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
+        internal static System.Drawing.Icon Online {
+            get {
+                object obj = ResourceManager.GetObject("Online", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
+        internal static System.Drawing.Icon OnLineBusy {
+            get {
+                object obj = ResourceManager.GetObject("OnLineBusy", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
         internal static System.Drawing.Bitmap run {
